8cd8fc1c2ea36a77d6392ca06dd8a0f9e496ea8c
Andrew Bowell <49959927+AnBowell@users.noreply.github.com>
2022-02-24 20:49:21 +0000

    fix: :bug: Silly mistake - sys.platform is a string not func.

--- eo_wrapper/EOkit/EOkit/__init__.py
+++ eo_wrapper/EOkit/EOkit/__init__.py
@@ -8,7 +8,7 @@ import sys
 # The EOkit folder is built when pip installed. This folder would normally
 # be git ignored; however, when building on ReadTheDocs, I need the .so file
 # and whatnot. 
-if sys.platform() != "win32":
+if sys.platform != "win32":
     lib = ""    
 else:
     lib = open(os.path.join(os.path.dirname(__file__), "native.so"))
