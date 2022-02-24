3494d8b1351a3f896aa2feeabd7d2f787d8f2610
Andrew Bowell <49959927+AnBowell@users.noreply.github.com>
2022-02-24 20:47:03 +0000

    fix: :bug: Try and fix the bug in the doc builder.

--- eo_wrapper/EOkit/EOkit/__init__.py
+++ eo_wrapper/EOkit/EOkit/__init__.py
@@ -2,6 +2,15 @@ __all__ = ["lib", "ffi"]
 
 import os
 from .ffi import ffi
+import sys
 
-lib = ffi.dlopen(os.path.join(os.path.dirname(__file__), "native.so"))
+
+# The EOkit folder is built when pip installed. This folder would normally
+# be git ignored; however, when building on ReadTheDocs, I need the .so file
+# and whatnot. 
+if sys.platform() != "win32":
+    lib = ""    
+else:
+    lib = open(os.path.join(os.path.dirname(__file__), "native.so"))
+    
 del os
