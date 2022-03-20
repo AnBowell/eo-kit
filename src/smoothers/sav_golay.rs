9e2d90221dfe99d6cfb5efed3708c5367fed23eb
Andrew Bowell <49959927+AnBowell@users.noreply.github.com>
2022-03-20 12:51:00 +0000

    feat: :sparkles: More progressn on the Sav-Golay.
    Switching over to nalgebra as it has more of the functions I need basically.

--- src/smoothers/sav_golay.rs
+++ src/smoothers/sav_golay.rs
@@ -1,11 +1,47 @@
-use ndarray::arr2;
+use nalgebra::DMatrix;
+
+fn factorial(num: i64) -> i64 {
+    (1..=num).product()
+}
 
 pub fn single_sav_golay(
     y_input_ptr: *mut f64,
     data_length: usize,
     window_size: i64,
     order: i64,
+    deriv: i64,
+    delta: f64,
 ) {
-    let order_range = 0..order + 1;
-    let half_window = (window_size as f64 - 1_f64 / 2_f64).floor();
+    let y_input: &mut [f64] = unsafe {
+        assert!(!y_input_ptr.is_null());
+        std::slice::from_raw_parts_mut(y_input_ptr, data_length)
+    };
+
+    let half_window = ((window_size as f64 - 1_f64) / 2_f64).floor() as i64;
+
+    let mut b_vec =
+        Vec::with_capacity(((half_window * 2) * (order + 1)) as usize);
+
+    for k in -half_window..half_window {
+        for i in 0..order + 1 {
+            b_vec.push((k as f64).powf(i as f64));
+        }
+    }
+
+    let b = DMatrix::from_vec(
+        (half_window * 2) as usize,
+        (order + 1) as usize,
+        b_vec,
+    );
+    let inverse_b = b.pseudo_inverse(1e-15).unwrap();
+
+    let row = inverse_b.row(deriv as usize)
+        * (delta.powf(deriv as f64))
+        * factorial(deriv) as f64;
+
+    // let first_vals = y_input[0] - (y_input[1..half_window + 1] - y_input[0]);
+    // let last_vals = y_input[data_length - 1]
+    //     - (y_input[-half_window - 1..-1] - y_input[-1]);
+
+    println!("Array: b {}", row);
 }
--- tests/unit/test_smoothers.rs
+++ tests/unit/test_smoothers.rs
@@ -2,7 +2,14 @@ use EOkit::smoothers::sav_golay::single_sav_golay;
 
 #[test]
 fn test_sav_golay_filter() {
-    let input_y: Vec<f64> = Vec::with_capacity(20);
+    let mut input_y: Vec<f64> = Vec::with_capacity(20);
+    for i in 0..20 {
+        input_y.push((i as f64).powf(2.));
+    }
 
-    let input_y_ptr = input_y.as_ptr();
+    let input_y_ptr = input_y.as_mut_ptr();
+
+    let data_length = input_y.len();
+
+    single_sav_golay(input_y_ptr, data_length, 4, 3, 0, 1.)
 }
