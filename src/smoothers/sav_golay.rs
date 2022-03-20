a4a40bcb09e1bae9065310f36534a7be12d04f3c
Andrew Bowell <49959927+AnBowell@users.noreply.github.com>
2022-03-20 10:36:15 +0000

    build: :heavy_plus_sign: Adding ndarry to the rust side. Starting on sav-golay filter.

--- src/smoothers/sav_golay.rs
+++ src/smoothers/sav_golay.rs
@@ -1,3 +1,11 @@
-pub fn single_sav_golay(y_input_ptr: *mut f64, data_length: usize, windows_size:usize) {
-    
+use ndarray::arr2;
+
+pub fn single_sav_golay(
+    y_input_ptr: *mut f64,
+    data_length: usize,
+    window_size: i64,
+    order: i64,
+) {
+    let order_range = 0..order + 1;
+    let half_window = (window_size as f64 - 1_f64 / 2_f64).floor();
 }
